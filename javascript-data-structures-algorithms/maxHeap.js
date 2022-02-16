cb18aa00dbda3ef038d2e248265701e106cf9bad
Jimmy <jameskinyua590@gmail.com>
2022-02-16 22:29:55 +0300

    Implement MaxHeap and MinHeap classes

---
+++ javascript-data-structures-algorithms/maxHeap.js
@@ -0,0 +1,50 @@
+const Heap = require("./heap.js");
+
+class MaxHeap extends Heap {
+  add(item) {
+	this.items.push(item);
+
+	this.bubbleUp();
+  }
+
+  poll() {
+	const item = this.items[0];
+
+	this.items[0] = this.items[this.items.length - 1];
+	this.items.pop();
+
+	this.bubbleDown();
+
+	return item;
+  }
+
+  bubbleDown() {
+	let idx = 0;
+
+	while (this.leftChild(idx) &&
+	  (this.leftChild(idx) > this.items[idx] ||
+		this.rightChild(idx) > this.items[idx])) {
+	  let biggerIdx = this.leftChildIdx(idx);
+
+	  if (this.rightChild(idx) &&
+		this.rightChild(idx) > this.items[biggerIdx]) {
+		biggerIdx = this.rightChildIdx(idx);
+	  }
+
+	  this.swap(biggerIdx, idx);
+
+	  idx = biggerIdx;
+	}
+  }
+
+  bubbleUp() {
+	let idx = this.items.length - 1;
+
+	while (this.parent(idx) &&
+	  this.parent(idx) < this.items[idx]) {
+	  this.swap(idx, this.parentIdx(idx));
+
+	  idx = this.parentIdx(idx);
+	}
+  }
+}
---
+++ javascript-data-structures-algorithms/minHeap.js
@@ -0,0 +1,47 @@
+const Heap = require("./heap.js");
+
+class MinHeap extends Heap {
+  bubbleDown() {
+	let idx = 0;
+
+	while (this.leftChild(idx) && this.leftChild(idx) < this.items[idx]) {
+	  let smallerIdx = this.leftChildIdx(idx);
+
+	  if (this.rightChild(idx) && this.rightChild(idx) < this.items[smallerIdx]) {
+		// if right is smaller, right swaps
+		smallerIdx = this.rightChildIdx(idx);
+	  }
+
+	  this.swap(smallerIdx, idx);
+
+	  idx = smallerIdx;
+	}
+  }
+
+  bubbleUp() {
+	let idx = this.items.length - 1;
+
+	while (this.parent(idx) && this.parent(idx) > this.items[idx]) {
+	  this.swap(this.parentIdx(idx), idx);
+
+	  idx = this.parentIdx(idx);
+	}
+  }
+
+  add(item) {
+	this.items.push(item);
+
+	this.bubbleUp();
+  }
+
+  poll() {
+	const item = this.items[0];
+
+	this.items[0] = this.items[this.items.length - 1];
+	this.items.pop();
+
+	this.bubbleDown();
+
+	return item;
+  }
+}
